5cca6691fb7d6024018a65076085e31dd0041dd0
Josh Dean <josdea@gmail.com>
2017-06-23 10:40:06 -0400

    added jump page files

---
+++ jump/js/index.js
@@ -0,0 +1,298 @@
+// before editing this page see the example unit file for a sample starter of the jumppage object
+
+var app = angular.module("jumpPage", []);
+app.controller("myCtrl", function ($scope) {
+  $scope.jumppage = {
+    "pageTitle": "Math 1530 Jump Page",
+    "units": [
+      {
+        "title": "Unit 1",
+        "sections": [
+          {
+            "title": "Section 1.1",
+            "links": [
+              {
+                "text": "Statistics Thing 1",
+                "url": "http://http://google.com",
+                "icon": "fa-bath "
+              },
+              {
+                "text": "Statistics Thing 2",
+                "url": "http://yahoo.com",
+                "icon": "fa-camera-retro"
+              }
+            ]
+          },
+          {
+            "title": "Section 2.12",
+            "links": [
+              {
+                "text": "Statistics Thing 2.1",
+                "url": "http://google.com",
+                "icon": "fa-wpexplorer"
+              },
+              {
+                "text": "Statistics Thing 2",
+                "url": "http://yahoo.com",
+                "desc":
+                "Some information and stuff 2 for link 2 in unit 1 and section 2",
+                "icon": "fa-telegram"
+              },
+              {
+                "text": "Statistics Thing 2.1",
+                "url": "http://google.com",
+                "desc":
+                "Some information and stuff for link 1 in section 2 in unit 1",
+                "icon": "fa-ravelry"
+              },
+              {
+                "text": "Statistics Thing 2",
+                "url": "http://yahoo.com",
+                "desc":
+                "Some information and stuff 2 for link 2 in unit 1 and section 2",
+                "icon": "fa-briefcase"
+              },
+              {
+                "text": "Statistics Thing 2.1",
+                "url": "http://google.com",
+                "desc":
+                "Some information and stuff for link 1 in section 2 in unit 1",
+                "icon": "fa-cogs"
+              },
+              {
+                "text": "Statistics Thing 2",
+                "url": "http://yahoo.com",
+                "desc":
+                "Some information and stuff 2 for link 2 in unit 1 and section 2",
+                "icon": "fa-database"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "title": "Unit 2",
+        "sections": [{ "title": "Section 2.1",
+        "links": [
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            }]},
+        {
+          "title": "Section 2.2",
+          "links": [
+            {
+              "text": "Statistics Thing 2.2",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-telegram"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-ravelry"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-briefcase"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-cogs"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-database"
+            }
+          ]
+        }]
+      },
+      {
+        "title": "Unit 3",
+        "sections": [{ "title": "Section 2.1",
+        "links": [
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            }]},
+        {
+          "title": "Section 2.2",
+          "links": [
+            {
+              "text": "Statistics Thing 2.2",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-telegram"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-ravelry"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-briefcase"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-cogs"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-database"
+            }
+          ]
+        }]
+      },
+      {
+        "title": "Unit 4",
+        "sections": [{ "title": "Section 2.1",
+        "links": [
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            }]},
+        {
+          "title": "Section 2.2",
+          "links": [
+            {
+              "text": "Statistics Thing 2.2",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-telegram"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-ravelry"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-briefcase"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-cogs"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-database"
+            }
+          ]
+        }]
+      },
+      {
+        "title": "Unit 5",
+        "sections": [{ "title": "Section 2.1",
+        "links": [
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            }]},
+        {
+          "title": "Section 2.2",
+          "links": [
+            {
+              "text": "Statistics Thing 2.2",
+              "url": "http://google.com",
+              "icon": "fa-wpexplorer"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-telegram"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-ravelry"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-briefcase"
+            },
+            {
+              "text": "Statistics Thing 2.1",
+              "url": "http://google.com",
+              "desc":
+              "Some information and stuff for link 1 in section 2 in unit 1",
+              "icon": "fa-cogs"
+            },
+            {
+              "text": "Statistics Thing 2",
+              "url": "http://yahoo.com",
+              "desc":
+              "Some information and stuff 2 for link 2 in unit 1 and section 2",
+              "icon": "fa-database"
+            }
+          ]
+        }]
+      }
+
+
+
+    ]
+  };
+});
