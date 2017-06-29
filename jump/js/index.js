a164f3bbc6bf33e5dcb84abaf8d0265c993c8fb1
Josh Dean <josdea@gmail.com>
2017-06-29 16:45:45 -0400

    updated structure

--- jump/js/index.js
+++ jump/js/index.js
@@ -1,298 +1,87 @@
-// before editing this page see the example unit file for a sample starter of the jumppage object
-
 var app = angular.module("jumpPage", []);
 app.controller("myCtrl", function ($scope) {
+  let type = ['Prepare','Learn','Practice','Check','Resources']; //define resource type
+  let icon = ['fa-play', 'fa-book']; //define icons
+  let media =['video', 'reading', 'audio']
+
   $scope.jumppage = {
-    "pageTitle": "Math 1530 Jump Page",
-    "units": [
-      {
-        "title": "Unit 1",
-        "sections": [
-          {
-            "title": "Section 1.1",
-            "links": [
-              {
-                "text": "Statistics Thing 1",
-                "url": "http://google.com",
-                "icon": "fa-bath "
-              },
-              {
-                "text": "Statistics Thing 2",
-                "url": "http://yahoo.com",
-                "icon": "fa-camera-retro"
-              }
-            ]
-          },
-          {
-            "title": "Section 2.12",
-            "links": [
-              {
-                "text": "Statistics Thing 2.1",
-                "url": "http://google.com",
-                "icon": "fa-wpexplorer"
-              },
-              {
-                "text": "Standard Deviation",
-                "url": "http://yahoo.com",
-                "desc":
-                "Some information and stuff 2 for link 2 in unit 1 and section 2",
-                "icon": "fa-telegram"
-              },
-              {
-                "text": "Statistics Thing 2.1",
-                "url": "http://google.com",
-                "desc":
-                "Some information and stuff for link 1 in section 2 in unit 1",
-                "icon": "fa-ravelry"
-              },
-              {
-                "text": "Statistics Thing 2",
-                "url": "http://yahoo.com",
-                "desc":
-                "Some information and stuff 2 for link 2 in unit 1 and section 2",
-                "icon": "fa-briefcase"
-              },
-              {
-                "text": "Statistics Thing 2.1",
-                "url": "http://google.com",
-                "desc":
-                "Some information and stuff for link 1 in section 2 in unit 1",
-                "icon": "fa-cogs"
-              },
-              {
-                "text": "Statistics Thing 2",
-                "url": "http://yahoo.com",
-                "desc":
-                "Some information and stuff 2 for link 2 in unit 1 and section 2",
-                "icon": "fa-database"
-              }
-            ]
-          }
-        ]
-      },
-      {
-        "title": "Unit 2",
-        "sections": [{ "title": "Section 2.1",
-        "links": [
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            }]},
+  "pageTitle": "Math 1530 Jump Page",
+  "units": [
+    {
+      "title": "Declarative Statistics", //required unit title
+      "topics": [
         {
-          "title": "Section 2.2",
-          "links": [
+          "title": "1.1 Topic something", //required topic title
+          "objectives": [
             {
-              "text": "Statistics Thing 2.2",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-telegram"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-ravelry"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-briefcase"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-cogs"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-database"
-            }
-          ]
-        }]
-      },
-      {
-        "title": "Unit 3",
-        "sections": [{ "title": "Section 2.1",
-        "links": [
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            }]},
+              "title": "1.1a Objective of Something", //optional objective title
+              "links": [
+                {
+                  "icon": icon[1], // required - refers to array above - first value is icon[0] - no quotes
+                  "class": "", //required but can be blank - for extra css styling
+                  "text": "Something about distribution", //required button text 
+                  "url": "https://yahoo.com", //required
+                  "type": type[0], // required - refers to array above - first value is type[0] - no quotes
+                  "media": media[0], // required - refers to array above - first value is media[0] - no quotes
+                  "time": "10:30", // optional - completion time
+                  "tags": ['khan academy', 'z-values']//optional tags
+                } // end of link object - add a comma to add another link
+              ] // end of links array
+            } // end of an objective - add a comma here to add anohter objective
+          ] // end of objectives array
+        } // end of topic object - add a comma here to add another topic
+      ] // end of topics array
+    }, // end of unit object - add a comma to add another unit // end of unit object - add a comma to add another unit
+     {
+      "title": "Another Stats Unit", //required unit title
+      "topics": [
         {
-          "title": "Section 2.2",
-          "links": [
+          "title": "2.1 Topic something", //required topic title
+          "objectives": [
             {
-              "text": "Statistics Thing 2.2",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-telegram"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-ravelry"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-briefcase"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-cogs"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-database"
-            }
-          ]
-        }]
-      },
-      {
-        "title": "Unit 4",
-        "sections": [{ "title": "Section 2.1",
-        "links": [
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            }]},
-        {
-          "title": "Section 2.2",
-          "links": [
-            {
-              "text": "Statistics Thing 2.2",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-telegram"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-ravelry"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-briefcase"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-cogs"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-database"
-            }
-          ]
-        }]
-      },
-      {
-        "title": "Unit 5",
-        "sections": [{ "title": "Section 2.1",
-        "links": [
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            }]},
-        {
-          "title": "Section 2.2",
-          "links": [
-            {
-              "text": "Statistics Thing 2.2",
-              "url": "http://google.com",
-              "icon": "fa-wpexplorer"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-telegram"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-ravelry"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-briefcase"
-            },
-            {
-              "text": "Statistics Thing 2.1",
-              "url": "http://google.com",
-              "desc":
-              "Some information and stuff for link 1 in section 2 in unit 1",
-              "icon": "fa-cogs"
-            },
-            {
-              "text": "Statistics Thing 2",
-              "url": "http://yahoo.com",
-              "desc":
-              "Some information and stuff 2 for link 2 in unit 1 and section 2",
-              "icon": "fa-database"
-            }
-          ]
-        }]
-      }
+              "title": "2.1a Objective of Something", //optional objective title
+              "links": [
+                {
+                  "icon": icon[0], // required - refers to array above - first value is icon[0] - no quotes
+                  "class": "", //required but can be blank - for extra css styling
+                  "text": "Sample Button", //required 
+                  "url": "https://yahoo.com", //required
+                  "type": type[1], // required - refers to array above - first value is type[0] - no quotes
+                  "media": media[1], // required - refers to array above - first value is media[0] - no quotes
+                  "time": "4:12", // optional - completion time
+                  "tags": ['khan academy', 'z-values']//optional tags
+                }, // end of link object - add a comma to add another link
+                 {
+                  "icon": icon[0], // required - refers to array above - first value is icon[0] - no quotes
+                  "class": "", //required but can be blank - for extra css styling
+                  "text": "Sample Button 2", //required 
+                  "url": "https://yahoo.com", //required
+                  "type": type[1], // required - refers to array above - first value is type[0] - no quotes
+                  "media": media[1], // required - refers to array above - first value is media[0] - no quotes
+                  "time": "4:12", // optional - completion time
+                  "tags": ['khan academy', 'z-values']//optional tags
+                } // end of link object - add a comma to add another link
+              ] // end of links array
+            } // end of an objective - add a comma here to add anohter objective
+          ] // end of objectives array
+        } // end of topic object - add a comma here to add another topic
+      ] // end of topics array
+    } // end of unit object - add a comma to add another unit // end of unit object - add a comma to add another unit
+  ] // end of units array
+} // end of JSON object
 
+ 
+});
 
+// Converts tag array into comma separated list
+app.filter('join', function () {
+    return function join(array, separator, prop) {
+        if (!Array.isArray(array)) {
+            return array; 
+        }
 
-    ]
-  };
+        return (!!prop ? array.map(function (item) {
+            return item[prop];
+        }) : array).join(separator);
+    };
 });
